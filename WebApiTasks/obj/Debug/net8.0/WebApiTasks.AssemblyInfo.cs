2d0630f04fdd58dc7b7810cd11a6869068fca278
Aws-Shraideh <amalshraiedeh21@cit.just.edu.jo>
2024-08-12 13:44:28 +0300

    Commit Message

--- WebApiTasks/obj/Debug/net8.0/WebApiTasks.AssemblyInfo.cs
+++ WebApiTasks/obj/Debug/net8.0/WebApiTasks.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiTasks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ccad6dba642da4dfbb2ca63e07dcb2e062c11c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ea49d7ebc2affcd8b1251035a4c602bfb1aafee")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiTasks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiTasks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
